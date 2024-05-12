d7d536a659cd308899a7b21db962d27ec02ac5c4
LADDA28 <150544946+ladda28@users.noreply.github.com>
2024-05-12 23:41:24 +0530

    Add files via upload

---
+++ TaskManagement/src/main/java/com/TaskManagement/Controller/TaskController.java
@@ -0,0 +1,36 @@
+package com.TaskManagement.Controller;
+
+import com.TaskManagement.Dto.TaskDto;
+import com.TaskManagement.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("task")
+public class TaskController {
+@Autowired
+TaskService taskService;
+    @PostMapping("{id}/task")
+    public ResponseEntity<TaskDto> createTasks(@PathVariable (name = "id") long userid , @RequestBody TaskDto taskDto){
+    return new ResponseEntity<>( taskService.createTask(userid,taskDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{userid}/tasks")
+    public ResponseEntity<List<TaskDto>> getalltasks(@PathVariable (name = "userid") long userid ){
+        return new ResponseEntity<>( taskService.getAllTasks(userid),HttpStatus.OK);
+    }
+    @GetMapping("{userid}/task/{taskid}")
+    public ResponseEntity< TaskDto> getSpecificTask(@PathVariable (name = "userid")long userid ,@PathVariable(name = "taskid") long taskid){
+        return new ResponseEntity<>( taskService.getTask(userid,taskid),HttpStatus.OK);
+    }
+
+    @DeleteMapping("{userid}/task/{taskid}")
+    public ResponseEntity<String> deleteTask(@PathVariable (name = "userid") long userid,@PathVariable (name = "taskid") long taskid){
+        taskService.deleteTask(userid,taskid);
+    return new ResponseEntity<>(String.format("Taskid %d is Deleted",taskid),HttpStatus.OK);
+    }
+}
---
+++ TaskManagement/src/main/java/com/TaskManagement/Dto/TaskDto.java
@@ -0,0 +1,11 @@
+package com.TaskManagement.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDto {
+    private long id;
+    private String taskname;
+}
